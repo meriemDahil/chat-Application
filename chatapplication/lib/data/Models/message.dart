37bb76f35ed758d62d6dd55d0bf9b731c0aa36b6
meriem-da <meriemdahil31@gmail.com>
2024-05-31 23:49:38 +0200

    messageModel added

---
+++ chatapplication/lib/data/Models/message.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatMessage {
+  final String id;
+  final String senderId;
+  final String receiverId;
+  final String message;
+  final Timestamp timestamp;
+
+  ChatMessage({
+    required this.id,
+    required this.senderId,
+    required this.receiverId,
+    required this.message,
+    required this.timestamp,
+  });
+
+  factory ChatMessage.fromDocument(DocumentSnapshot doc) {
+    return ChatMessage(
+      id: doc.id,
+      senderId: doc['senderId'],
+      receiverId: doc['receiverId'],
+      message: doc['message'],
+      timestamp: doc['timestamp'],
+    );
+  }
+
+  Map<String, dynamic> toDocument() {
+    return {
+      'senderId': senderId,
+      'receiverId': receiverId,
+      'message': message,
+      'timestamp': timestamp,
+    };
+  }
+}
